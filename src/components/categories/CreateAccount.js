394d6217ddb3aba07c70882119eb10206bc2a950
Mwenje <mwenje24@gmail.com>
2022-09-09 07:12:38 +0300

    created a organizers sigup form

--- src/components/categories/CreateAccount.js
+++ src/components/categories/CreateAccount.js
@@ -4,7 +4,25 @@ import {Link} from 'react-router-dom'
 const CreateAccount = () => {
     return (
         <>
-          
+          <form className="modal-content animate" onSubmit="">
+          <span className='page-headings'>create account</span>
+          <div className="log-container">
+          <label className='form-label mt-4'><b>organiser name</b></label>
+            <input className="form-control" type="text" placeholder="Enter Name" name="organizer-name"/>
+
+            <label className='form-label'><b>email</b></label>
+            <input className="form-control" type="text" placeholder="Enter Email" name="email"/>
+
+            <label className='form-label'><b>Password</b></label>
+            <input className="form-control" type="password" placeholder="Enter Password" name="psw"/>
+              
+            <button className='btn btn-sm btn-outline-secondary mt-3 w-25 px-5' type="submit"><Link to="/login" className='btn-sm'> Submit</Link></button>
+          </div>
+
+          <div className="log-container">
+            <span className="psw">Don't have an Account <Link to="/create-account" className='nav-header-link'> Sign Up </Link></span>
+          </div>
+        </form>
         </>
     )
 }
